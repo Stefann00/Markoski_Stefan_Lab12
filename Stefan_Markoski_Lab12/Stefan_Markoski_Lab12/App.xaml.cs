45b7c23869d13e6e0986856f5c54168f7689f894
Stefan Markoski <stefan.markoski00@gmail.com>
2022-01-16 13:27:55 +0100

    Add project files.

---
+++ Stefan_Markoski_Lab12/Stefan_Markoski_Lab12/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Stefan_Markoski_Lab12.Data;
+
+namespace Stefan_Markoski_Lab12
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
